dedb24fe8393a722fecd7b713a0a12a6d26d760d
arunt <arunt2005@gmail.com>
2025-11-10 17:35:36 +0530

    Initial commit - Spring Boot first-app

--- src/main/java/com/apps/FirstAppApplication.java
+++ src/main/java/com/apps/FirstAppApplication.java
@@ -3,6 +3,7 @@ package com.apps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class FirstAppApplication {
 
